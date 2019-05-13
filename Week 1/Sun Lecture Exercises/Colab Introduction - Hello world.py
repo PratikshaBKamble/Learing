e40c6b35222e551449dc9f6e63476657bcc781ab
PratikshaBKamble <50519549+PratikshaBKamble@users.noreply.github.com>
2019-05-13 20:03:53 +0530

    Colab Introduction
    #Colab introduction - Hello World
    #Addition of two numbers

---
+++ Week 1/Sun Lecture Exercises/Colab Introduction - Hello world.py
@@ -0,0 +1,8 @@
+#Colab introduction - Hello World
+print("Hello World!!!!!!!!!!!!!")
+
+#Addition of two numbers
+a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+c=a+b
+print("Addition of two numbers is ",c)
