fefaa6e2c0ef28cabcaa3fe8b8b7f01ed74d98b2
PratikshaBKamble <50519549+PratikshaBKamble@users.noreply.github.com>
2019-05-13 20:10:47 +0530

    Add files via upload

---
+++ Week 1/Sun Lecture Exercises/Nested if else.py
@@ -0,0 +1,22 @@
+
+#I will do nested if and else
+
+a=int(input("Enter A :"))
+b=int(input("Enter B :"))
+c=int(input("Enter C :"))
+            
+if(a>b):
+            if(a>c):
+                    print("A is greatest : ")
+            else:
+                    print("C is grestest : ")
+else:
+            if(b>c):
+                  print("B is greatest : ")
+            else:
+                  print("C is greatest : ")
+
+
+
+
+
