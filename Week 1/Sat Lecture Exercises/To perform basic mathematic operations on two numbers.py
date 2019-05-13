e3d8b60d5a2072b7b6edb2117133d86133b714ce
PratikshaBKamble <50519549+PratikshaBKamble@users.noreply.github.com>
2019-05-13 11:34:12 +0530

    Add files via upload
    to upload a basic program to perform different mathematics operations on two numbers

---
+++ Week 1/Sat Lecture Exercises/To perform basic mathematic operations on two numbers.py
@@ -0,0 +1,16 @@
+
+a=int(input("kindly enter 1st number"))
+b=int(input("kindly enter 2nd number"))
+c=a+b
+d=a*b
+e=a-b
+f=a/b
+
+print("Addition of two number is",c)
+print("Product of two number is",d)
+print("Substraction of two number is",e)
+print("Division of two number is",f)
+print(type(f))
+print(type(c))
+print(type(d))
+print(type(e))
