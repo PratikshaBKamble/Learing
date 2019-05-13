d2c38c8844caae7e9eaa71ea786930fd6377e744
PratikshaBKamble <50519549+PratikshaBKamble@users.noreply.github.com>
2019-05-13 19:32:57 +0530

    Add files via upload

---
+++ Week 1/Sat Lecture Exercises/Greater than operator used.py
@@ -0,0 +1,11 @@
+a=int(input("Enter first number"))
+b=float(input("Enter second number"))
+c=str(input("Enter your name"))
+d=a>b
+print(d)
+
+a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+
+c=(a+b)/(a-b)
+print("a is{}".format(c))
