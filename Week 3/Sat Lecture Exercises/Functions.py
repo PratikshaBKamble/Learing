d26eaec8d4ab3871574f67d53a95b298b94fa9b7
PratikshaBKamble <50519549+PratikshaBKamble@users.noreply.github.com>
2019-05-13 23:13:06 +0530

    Add files via upload

---
+++ Week 3/Sat Lecture Exercises/Functions.py
@@ -0,0 +1,49 @@
+
+Days=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+Month=["January","February","March","April","May","June","July","August","September","October","November","December"]
+name=str(input("Please enter your name:"))
+dob=str(input("Please enter date of birth (for egs.13031994)"))
+current_year=str(input("Please enter current year (for egs.2019)"))
+year=dob[4:8]
+age=int(current_year) - int(year)
+print("Your age is :",age)
+
+''''
+x=str(input("Month of which you want to enquire no of days:"))
+if x in Month:
+    if
+
+for i in range(1,32):
+    print(i)
+
+def summation(a,b):
+    c = a+b
+    return c
+
+print(summation(78,36))
+def sum(a,b):
+  c=a+b
+  return (c)
+sum(10,20)
+
+def sub(a,b):
+  d=a-b
+  return print(d)
+
+def mul(a,b):
+  e=a*b
+  return print(e)
+
+def div(a,b):
+  f=a/b
+  return print(f)
+
+sub(10,20)
+mul(10,20)
+div(10,20)
+
+x="january"
+y=print(x.capitalize())
+y=print(x.upper())
+y=print(x.lower())
+'''
