1f2699e40614f486bfa0ffa9cfb821d85d161a8d
PratikshaBKamble <50519549+PratikshaBKamble@users.noreply.github.com>
2019-05-13 19:33:57 +0530

    Add files via upload

---
+++ Week 1/Sat Lecture Exercises/Operators using format method.py
@@ -0,0 +1,23 @@
+
+a=int(input("Enter value for a"))
+b=int(input("Enter value for b"))
+c=int(input("Enter value for c"))
+d=int(input("Enter value for d"))
+e=int(input("Enter value for e"))
+f=int(input("Enter value for f"))
+g=int(input("Enter value for g"))
+h=int(input("Enter value for h"))
+
+i=a+b
+j=c-d
+k=e*f
+l=g/h
+
+L=i>j
+M=k<l
+
+print("add is {},sub is {},product is {},divison is {} L is {} and M is {}".format(i,j,k,l,L,M))
+
+
+
+
