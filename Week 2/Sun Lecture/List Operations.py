3dc88f558d1f6579a15c6f1f93ec03b8b3ef072e
PratikshaBKamble <50519549+PratikshaBKamble@users.noreply.github.com>
2019-05-13 23:03:52 +0530

    Add files via upload

---
+++ Week 2/Sun Lecture/List Operations.py
@@ -0,0 +1,22 @@
+'''
+name = str(input("Enter your name"))
+Y = str(input("Enter the letters that you want to append"))
+Z= int(input("Enter the no of initials to your nickname"))
+
+nickname =name[0:Z] +Y
+reverse=name[0:0:-1]
+print("Your nickname is:",nickname)
+print("Your reverse is:",reverse)
+'''
+
+List = ["Pratiksha","Ishika","Jovin","Dipesh","Nitin","Dipesh","Vedant"]
+List[2]
+
+'''
+list =["Pratiksha"]
+Y = str(input("Enter the letters that you want to append"))
+Z= int(input("Enter the no of initials to your nickname"))
+
+nickname = list[0:Z]+Y
+print("Your nickname is:",nickname)
+'''
