a33aa4740000e3f1c8a9d5fa1bb746f6c69f8be2
PratikshaBKamble <50519549+PratikshaBKamble@users.noreply.github.com>
2019-05-14 22:46:50 +0530

    Add files via upload

---
+++ Solutions/OOP's Concept Code.py
@@ -0,0 +1,16 @@
+print("OOP's Concepts \n 1.Class \n 2.Objects \n 3.Data Abstraction \n 4.Inheritance \n 5.Encapsulation \n 6.Plymorphism")
+choice=int(input("Please select your choice:"))
+if(choice==1):
+    print("Classes — the definitions for the data format and available procedures for a given type or class of object; may also contain data and procedures (known as class methods) themselves, i.e. classes contain the data members and member functions")
+elif(choice==2):
+    print("Objects — instances of classes")
+elif(choice==3):
+    print("Data abstraction — refers to providing only essential information to the outside world and hiding their background details.")
+elif(choice==4):
+    print("Inheritance — is when an object or class is based on another object or class using the same implementation.")
+elif(choice==5):
+    print("Encapsulation — is an object-oriented programming concept that binds together the data and functions that manipulate the data, and that keeps both safe from outside interference and misuse. Data encapsulation led to the important OOP concept of data hiding")
+elif(choice==6):
+    print("Encapsulation — is an object-oriented programming concept that binds together the data and functions that manipulate the data, and that keeps both safe from outside interference and misuse. Data encapsulation led to the important OOP concept of data hiding")
+else:
+    print("Invalid input")
